5a193fde5da019cf2846061d34b5aafbff2826fe
Slava Skryabin <vs8199@gmail.com>
2019-11-23 02:42:52 -0800

    Initial commit

--- src/test/java/pages/QuoteForm.java
+++ src/test/java/pages/QuoteForm.java
@@ -3,8 +3,6 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import static support.TestContext.getExecutor;
-
 public class QuoteForm extends Page {
 
     public QuoteForm() {
