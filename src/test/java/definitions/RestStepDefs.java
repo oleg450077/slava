e094c7f0addf8133565a7115b93353c9e0198121
Slava Skryabin <vs8199@gmail.com>
2019-12-13 01:24:50 -0800

    Day 17

--- src/test/java/definitions/RestStepDefs.java
+++ src/test/java/definitions/RestStepDefs.java
@@ -2,10 +2,13 @@ package definitions;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import support.RestWrapper;
 
+import java.util.List;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getData;
 
 public class RestStepDefs {
@@ -13,5 +16,33 @@ public class RestStepDefs {
     @Given("I login via REST as {string}")
     public void iLoginViaRESTAs(String role) {
         new RestWrapper().login(getData(role));
+    }
+
+    @When("I create via REST {string} position")
+    public void iCreateViaRESTPosition(String type) {
+        new RestWrapper().createPosition(getData(type));
+    }
+
+    @Then("I verify via REST new position is in the list")
+    public void iVerifyViaRESTNewPositionIsInTheList() {
+        List<Map<String, Object>> actualPositions = new RestWrapper().getPositions();
+        Map<String, Object> lastPosition = RestWrapper.getLastPosition();
+
+        boolean isFound = false;
+        for (Map<String, Object> actualPosition : actualPositions) {
+            if (actualPosition.get("id").equals(lastPosition.get("id"))) {
+                isFound = true;
+                for (String key : lastPosition.keySet()) {
+                    Object expected = lastPosition.get(key);
+                    Object actual = actualPosition.get(key);
+                    System.out.println("Verifying field: " + key);
+                    System.out.println("Expected: " + expected);
+                    System.out.println("Actual: " + actual);
+                    assertThat(actual).isEqualTo(expected);
+                }
+                break;
+            }
+        }
+        assertThat(isFound).isTrue();
     }
 }
--- src/test/java/support/RestWrapper.java
+++ src/test/java/support/RestWrapper.java
@@ -4,15 +4,24 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 public class RestWrapper {
 
     private String baseUrl = "https://skryabin.com/recruit/api/v1/";
     private static String loginToken;
+    private static Map<String, Object> lastPosition;
 
     public static final String CONTENT_TYPE = "Content-Type";
     public static final String JSON = "application/json";
+    public static final String AUTH = "Authorization";
+
+    public static Map<String, Object> getLastPosition() {
+        return lastPosition;
+    }
 
     public void login(Map<String, String> credentials) {
 
@@ -40,5 +49,67 @@ public class RestWrapper {
 
         loginToken = "Bearer " + result.get("token");
 
+    }
+
+    public Map<String, Object> createPosition(Map<String, String> position) {
+
+        String dateOpen = position.get("dateOpen");
+        String isoDateOpen = new SimpleDateFormat("yyyy-MM-dd").format(new Date(dateOpen));
+        position.put("dateOpen", isoDateOpen);
+
+        String title = position.get("title");
+        String uniqueTitle = title + new SimpleDateFormat("-yyyy-MM-dd-HH-mm-sss").format(new Date());
+        position.put("title", uniqueTitle);
+
+        // prepare
+        RequestSpecification request = RestAssured
+                .given()
+                .log().all()
+                .baseUri(baseUrl)
+                .header(CONTENT_TYPE, JSON)
+                .header(AUTH, loginToken)
+                .body(position);
+
+        // execute
+        Response response = request
+                .when()
+                .post("positions");
+
+        // verify and extract
+        Map<String, Object> result = response
+                .then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .jsonPath()
+                .getMap("");
+
+        lastPosition = result;
+
+        return result;
+    }
+
+    public List<Map<String, Object>> getPositions() {
+        // prepare
+        RequestSpecification request = RestAssured
+                .given()
+                .baseUri(baseUrl)
+                .log().all();
+
+        // execute
+        Response response = request
+                .when()
+                .get("positions");
+
+        // verify and extract
+        List<Map<String, Object>> result = response
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getList("");
+
+        return result;
     }
 }
